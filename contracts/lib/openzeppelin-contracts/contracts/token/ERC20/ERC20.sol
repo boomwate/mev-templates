e1474e245dd4e0fefb43077fbe01f6df87646284
boomwate <68445610+boomwate@users.noreply.github.com>
2024-01-12 17:36:20 -0500

    Update ERC20.sol
    1

--- contracts/lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol
+++ contracts/lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v4.9.0) (token/ERC20/ERC20.sol)
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.13;
 
 import "./IERC20.sol";
 import "./extensions/IERC20Metadata.sol";
-import "../../utils/Context.sol";
+import "./../../utils/Context.sol";
 
 /**
  * @dev Implementation of the {IERC20} interface.
